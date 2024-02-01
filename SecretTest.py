3e31075d4431e410310ca13adef140cd72e767ca
tmichaud-moveworks <tmichaud@moveworks.ai>
2024-02-01 13:45:02 -0600

    Create SecretTest.py

---
+++ SecretTest.py
@@ -0,0 +1,7 @@
+import stripe 
+
+stripe.api_key = "sk_live_4eC39HqLyjWDarjtT1zdp7dcTYooMQauvdEDq54NiTphI7jx"
+
+token= "gho_28axLS98lwZQr9wixIO3k7MstW2sD9Xp8saB1eO”
+
+api_key = "AIzaSyBJm7CoylH_zhLfmnLNoWDljBtVesNmSrw”
